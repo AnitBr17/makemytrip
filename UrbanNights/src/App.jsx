d61024ee04248c08182908afe98062419e378d25
anit baranwal <anitbaranwal2018@gmail.com>
2025-04-26 14:53:53 +0530

    changes

--- UrbanNights/src/App.jsx
+++ UrbanNights/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Home from './components/Home';
+import Footer from './components/Footer';
+import AllRooms from './components/pages/AllRooms';
+import RoomDetails from './components/pages/RoomDetails';
+
+const App = () => {
+  const isOwnerPath = useLocation().pathname.includes('/owner');
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms/>} />
+          <Route path="/rooms/:id" element={<RoomDetails/>} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Footer/>
+    
+    </div>
   )
 }
 
---
+++ UrbanNights/src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from './Hero'
+import FeatureDestination from './FeatureDestination'
+import ExclusiveOffers from './ExclusiveOffers'
+import Testimonial from './Testimonial'
+import NewsLetter from './NewsLetter'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      <FeatureDestination/>
+      <ExclusiveOffers/>
+      <Testimonial/>
+      <NewsLetter/>
+      
+    </div>
+  )
+}
+
+export default Home
